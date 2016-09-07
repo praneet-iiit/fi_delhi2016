9b054a4f99e947c9b0dadfed77f76f7c635c71d1
Peter <peter.muryshkih@iais.fraunhofer.de>
2016-09-07 14:52:27 +0200

    documentation updates. Added Docker flavour of the tutorial.

---
+++ keyrock.tutorial/config.js
@@ -0,0 +1,13 @@
+var config = {}
+
+config.idmURL = 'https://account.lab.fiware.org';
+config.client_id = '';
+config.client_secret = '';
+config.callbackURL = 'http://localhost/login';
+
+// Depending on Grant Type:
+// Authorization Code Grant: code
+// Implicit Grant: token
+config.response_type = 'code';
+
+module.exports = config;
