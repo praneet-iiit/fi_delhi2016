76464590acda0946f92e3106a28dae1f2f0b4ff6
praneet-iiit <praneet3g@gmail.com>
2016-11-26 14:52:02 +0530

    Update config.js

--- keyrock.tutorial/config.js
+++ keyrock.tutorial/config.js
@@ -1,9 +1,9 @@
 var config = {}
 
 config.idmURL = 'https://account.lab.fiware.org';
-config.client_id = '';
-config.client_secret = '';
-config.callbackURL = 'http://localhost/login';
+config.client_id = '1ee970a4a37241e38fef59e61a12a3e4';
+config.client_secret = '7293663224da4a4c99337388ab794096';
+config.callbackURL = 'http://localhost:3128/login';
 
 // Depending on Grant Type:
 // Authorization Code Grant: code
